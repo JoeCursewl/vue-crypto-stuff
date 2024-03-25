e2e248268b8f6938d60f1cf4349d5b706fb128b2
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-03-24 22:58:04 -0400

    Upload files

---
+++ src/components/CardLin.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+const lintted_name = ref("This is not good at all")
+</script>
+
+
+<template>
+
+
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
